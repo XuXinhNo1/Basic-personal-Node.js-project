eb534ee07b1759b1384953260759e4a6ac0f73ed
XuXinhNo1 <khaihungpro2005@gmail.com>
2025-09-29 23:59:31 +0700

    new change!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Vercel! This is my first Node.js deploy 🚀");
+  res.send("Hello netlify! This is my first Node.js deploy 🚀");
 });
 
 app.listen(PORT, () => {
