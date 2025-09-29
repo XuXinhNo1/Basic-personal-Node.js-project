ecdb9df83356b356362a9d9abc81305a22e8bb2a
XuXinhNo1 <khaihungpro2005@gmail.com>
2025-09-29 23:21:35 +0700

    hi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Render! This is my first Node.js deploy 🚀");
+  res.send("Hello Vercel! This is my first Node.js deploy 🚀");
 });
 
 app.listen(PORT, () => {
