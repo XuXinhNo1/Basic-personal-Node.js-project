c5a9454fc6342bab1e80e7ca748f84ad9cb59ec0
XuXinhNo1 <khaihungpro2005@gmail.com>
2025-09-29 22:47:42 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Render! This is my first Node.js deploy 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
